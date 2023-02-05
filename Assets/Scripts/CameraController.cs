dfbdb53494b426bb75df744b810fe984736b1bfc
Zane ODell <odellzan@msu.edu>
2023-02-04 19:02:08 -0500

    Finished prototype camera
    Camera for prototype is complete, with it following the player as it gets to the edge of the playing area, denoted by two invisible colliders.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,46 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
+    float speed = 14.5f;
     Camera playerCamera;
     GameObject player;
     GameObject playArea;
     Mesh playAreaMesh;
     GameObject LeftSide;
     GameObject RightSide;
+    float playAreaLength;
+    Rigidbody playerBody;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         playerCamera = GetComponent<Camera>();
         player = GameObject.Find("Player");
         playArea = GameObject.Find("PlayArea");
         LeftSide = GameObject.Find("LeftCollider");
         RightSide = GameObject.Find("RightCollider");
-        
+        playAreaLength = Mathf.Abs(LeftSide.transform.position.x - RightSide.transform.position.x);
+        playerBody = player.GetComponent<Rigidbody>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         CameraPositioning();
+        
     }
 
     void CameraPositioning()
     {
         
         //Use the collider's position to find the edges and do the math from there. 
-        if (Mathf.Abs(player.transform.position.x) > 0) 
+        if (player.transform.position.x > (playAreaLength/2 - 0.2f * (playAreaLength)) ||
+            player.transform.position.x < (-playAreaLength / 2 + 0.2f * (playAreaLength)))
         {
-            MoveCamera(player.transform.position.x - 0 * Time.deltaTime);
+            if (Mathf.Abs(playerBody.velocity.magnitude) > 17)
+            {
+                MoveCamera(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            }
+            
+        }
+        else
+        {
+            MoveCamera(-transform.position.x, -transform.position.z);
         }
     }
 
-    void MoveCamera(float delta)
+    void MoveCamera(float x, float y)
     {
-        playerCamera.transform.Translate(delta, 0, 0);
+        Vector3 movementAmount = new Vector3(x, 0, y) * speed * Time.deltaTime;
+        transform.Translate(movementAmount);
     }
 }
