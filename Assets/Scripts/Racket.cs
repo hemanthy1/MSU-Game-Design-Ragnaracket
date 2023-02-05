81b42b44e6a12bfa823e73d3229c04f9e8843967
Hemanth Yalamanchili <52936630+hemanthy1@users.noreply.github.com>
2023-02-05 16:54:32 -0500

    PlayerHit and updated Racket script

---
+++ Assets/Scripts/Racket.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Shuttlecock")
+        {
+          
+            other.transform.parent.GetComponent<ShuttlecockMotion>().NextTarget();
+        }
+    }
+}
