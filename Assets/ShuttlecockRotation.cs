8a70e25166ed367f1f47fbc3c90450cdd9868aee
anc08 <124193717+anc08@users.noreply.github.com>
2023-02-06 11:48:44 -0500

    spinning

--- Assets/ShuttlecockRotation.cs
+++ Assets/ShuttlecockRotation.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ShuttlecockRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool reverseSpin = false;
+    public float rotationSpeed = 0.25f;
+    private Vector3 rotationVector;
+
     void Start()
     {
-        
+        rotationVector = new Vector3(0, 0, rotationSpeed);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (transform.parent.GetComponent<ShuttlecockMotion>().IsMoving())
+        {
+            if (reverseSpin)
+                transform.Rotate(-rotationVector);
+            else
+                transform.Rotate(rotationVector);
+        }
     }
 }
